4fe40cfe64628782cf51553fb4f50fb86f51cef2
taha1337 <t.mousnia@gmail.com>
2026-04-29 23:00:06 +0100

    interacting with sdk

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,9 @@ from .models.options import Options
 from pydantic import ValidationError
 from .models.prompt import Prompt
 from .models.func_definition import FuncDefinition
+from typing import cast
+from llm_sdk import Small_LLM_Model
+from json import loads
 
 
 def main():
@@ -15,9 +18,12 @@ def main():
             try:
                 # parse json data from input files and validate it with
                 # pydantic models
-                prompts: list[Prompt] = parseJsonData(options.input, Prompt)
-                func_defs: list[FuncDefinition] = parseJsonData(
-                    options.functions_definition, FuncDefinition
+                prompts: list[Prompt] = cast(
+                    list[Prompt], parseJsonData(options.input, Prompt)
+                )
+                func_defs: list[FuncDefinition] = cast(
+                    list[FuncDefinition],
+                    parseJsonData(options.functions_definition, FuncDefinition),
                 )
                 if len(func_defs) == 0 or len(prompts) == 0:
                     print(
@@ -25,8 +31,21 @@ def main():
                     )
                     return
                 else:
-                    print("treating prompts...")
-                
+                    print(f"treating prompts... [{prompts[2].prompt}]")
+                    model = Small_LLM_Model()
+                    vocab: list[tuple[str, str]] = []
+                    with open(model.get_path_to_vocab_file(), "r") as vocabfile:
+                        # read json file and convert the dictionary data to a list of tuples that contain vocab and its token
+                        vocab = list(loads(vocabfile.read()).items())
+
+                    tokens: list[int] = model.encode(prompts[2].prompt)[0].tolist()
+                    print(tokens)
+                    while True:
+                        logits = model.get_logits_from_input_ids(tokens)
+                        max_logit_index = logits.index(max(logits))
+                        _, max_logit = vocab[max_logit_index]
+                        tokens.append(max_logit)
+                        print(model.decode(tokens))
             except ValidationError:
                 print("Error parsing JSON data")
                 return
